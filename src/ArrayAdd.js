23166fa284417b23072b227b09f0d6ebc20f4b1b
woong2fighting <hiit9@hanmail.net>
2023-03-18 16:06:43 +0900

    Array 연습

---
+++ src/ArrayAdd.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function  ArrayAdd({username, email, onChange, onCreateClick}){
+    return(
+        <div>
+            <input name="username"
+            placeholder="계정명"
+            onChange={onInputChange} 
+            value={username}/>
+
+            <input name="email"
+            placeholder="이메일"
+            onChange={onInputChange}
+            value={email}
+            />
+
+            <button onClick={onCreateClick}>등록</button>
+        </div>
+    )
+}
+console.log(onChange)
+export default ArrayAdd;
